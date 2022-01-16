7e83c90bbc58d6f135f7d7278c869154474d1aa6
eliastxt <eliastxt@gmail.com>
2022-01-16 13:21:07 -0300

    Projeto criado

---
+++ src/main/java/com/rodademate/stocksrm/StocksrmApplication.java
@@ -0,0 +1,13 @@
+package com.rodademate.stocksrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodademate/stocksrm/StocksrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodademate.stocksrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
